2825edc2a9c1ed326d1fd00be8ca46b11fabb320
Craig <indigomx9@gmail.com>
2022-04-22 14:12:50 -0400

    Updated Portfolio page.

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,10 +5,23 @@ export const Portfolio = (): JSX.Element => {
     return (
         <React.Fragment>
             <h1>Portfolio</h1>
+            <h3>Cards with links</h3>
             <main className="container">
-                <aside className="box-1">One</aside>
-                <aside className="box-2">Two</aside>
-                <aside className="box-3">Three</aside>
+                <section className="box-1">
+                    <aside className="section-header">One</aside>
+                    <aside className="section-body">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit consequatur modi earum quod quisquam error odio dignissimos, ipsum ad.</aside>
+                    <aside className="section-footer">one</aside>
+                </section>
+                <section className="box-2">
+                    <aside className="section-header">Two</aside>
+                    <aside className="section-body">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit consequatur modi earum quod quisquam error odio dignissimos, ipsum ad.</aside>
+                    <aside className="section-footer">two</aside>
+                </section>
+                <section className="box-3">
+                    <aside className="section-header">Three</aside>
+                    <aside className="section-body">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit consequatur modi earum quod quisquam error odio dignissimos, ipsum ad.</aside>
+                    <aside className="section-footer">three</aside>
+                </section>
             </main>
         </React.Fragment>
     );
